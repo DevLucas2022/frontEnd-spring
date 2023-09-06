ce52ef429d4c2439c60b1f058db718bbf9e91ab2
Lucas de Oliveira Santos <103695606+DevLucas2022@users.noreply.github.com>
2023-09-06 10:16:08 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import ConsultaCatalogo from "./componentes/ConsultaCatalogo/index";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Fatec Diadema</h1>
+      <ConsultaCatalogo />
+    </div>
+  );
+}
